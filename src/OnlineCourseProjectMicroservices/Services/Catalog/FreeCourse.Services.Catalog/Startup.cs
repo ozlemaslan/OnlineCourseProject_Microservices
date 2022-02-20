e0cc3292f233a2f1c66d81308294cdc542314ee6
oaslan <oaslan@partner.innova.com.tr>
2022-02-20 18:25:49 +0300

    database and appSettings updated.

--- src/OnlineCourseProjectMicroservices/Services/Catalog/FreeCourse.Services.Catalog/Startup.cs
+++ src/OnlineCourseProjectMicroservices/Services/Catalog/FreeCourse.Services.Catalog/Startup.cs
@@ -1,3 +1,4 @@
+using FreeCourse.Services.Catalog.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -5,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
@@ -28,12 +30,18 @@ namespace FreeCourse.Services.Catalog
 
             services.AddControllers();
             services.AddAutoMapper(typeof(Startup));
+            services.Configure<DatabaseSettings>(Configuration.GetSection("DatabaseSettings"));
+
+            services.AddSingleton<IDatabaseSettings>(sp =>
+            {
+                return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FreeCourse.Services.Catalog", Version = "v1" });
             });
 
-            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
