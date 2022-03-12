501652ba57428e8f068082aa4d4a036d9c7be794
oaslan <oaslan@partner.innova.com.tr>
2022-03-12 16:18:37 +0300

    Basket API project created and Dto class writed.

---
+++ src/OnlineCourseProjectMicroservices/Services/Basket/FreeCourse.Services.Basket/Dtos/BasketDto.cs
@@ -0,0 +1,16 @@
+﻿namespace FreeCourse.Services.Basket.Dtos
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    public class BasketDto
+    {
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        public int? DiscountRate { get; set; } //İndirim oranı
+        public List<BasketItemDto> basketItems { get; set; }
+        public decimal TotalPrice
+        {
+            get => basketItems.Sum(x => x.Price * x.Quantity); //Ücret ile kaç tane kurs adedi varsa çarp hepsini topla 
+        }
+    }
+}
---
+++ src/OnlineCourseProjectMicroservices/Services/Basket/FreeCourse.Services.Basket/Dtos/BasketItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FreeCourse.Services.Basket.Dtos
+{
+    public class BasketItemDto
+    {
+        public int Quantity { get; set; } //adet
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public decimal Price { get; set; } //ücret
+    }
+}
