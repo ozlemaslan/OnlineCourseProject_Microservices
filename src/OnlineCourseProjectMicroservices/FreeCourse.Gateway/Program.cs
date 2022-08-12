7de54272cf166c67d4085b7c3c568d641931d094
oaslan <oaslan@innova.com.tr>
2022-08-12 14:00:30 +0300

    config setting done.

--- src/OnlineCourseProjectMicroservices/FreeCourse.Gateway/Program.cs
+++ src/OnlineCourseProjectMicroservices/FreeCourse.Gateway/Program.cs
@@ -1,11 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FreeCourse.Gateway
 {
@@ -17,8 +12,10 @@ namespace FreeCourse.Gateway
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+            Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((hostBuilder, config) =>
+            {
+                config.AddJsonFile($"configuration.{hostBuilder.HostingEnvironment.EnvironmentName}.json").AddEnvironmentVariables();
+            }).ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
