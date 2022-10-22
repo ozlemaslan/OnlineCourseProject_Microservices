6071843eaef8062b68a7a572faf31455f5bd0492
oaslan <oaslan@innova.com.tr>
2022-10-22 20:39:12 +0300

    value object created.

--- src/OnlineCourseProjectMicroservices/Services/Order/FreeCource.Services.Order.Domain/OrderAggregate/Address.cs
+++ src/OnlineCourseProjectMicroservices/Services/Order/FreeCource.Services.Order.Domain/OrderAggregate/Address.cs
@@ -1,7 +1,36 @@
-﻿namespace FreeCource.Services.Order.Domain.OrderAggregate
+﻿using FreeCourse.Services.Order.Domain.Core;
+using System.Collections.Generic;
+
+namespace FreeCource.Services.Order.Domain.OrderAggregate
 {
-    public class Address
+    public class Address : ValueObject
     {
-        public string Province { get; set; }
+        public string Province { get; private set; }
+
+        public string District { get; private set; }
+
+        public string Street { get; private set; }
+
+        public string ZipCode { get; private set; }
+
+        public string Line { get; private set; }
+
+        public Address(string province, string district, string street, string zipCode, string line)
+        {
+            Province = province;
+            District = district;
+            Street = street;
+            ZipCode = zipCode;
+            Line = line;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Province;
+            yield return District;
+            yield return Street;
+            yield return ZipCode;
+            yield return Line;
+        }
     }
 }
