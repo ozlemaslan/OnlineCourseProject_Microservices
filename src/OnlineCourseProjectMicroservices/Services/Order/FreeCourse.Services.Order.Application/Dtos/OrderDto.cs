cddb36d86bb65aa7b02350ab4a657e39018f5a6d
oaslan <oaslan@innova.com.tr>
2022-10-23 16:32:22 +0300

    order app created.

---
+++ src/OnlineCourseProjectMicroservices/Services/Order/FreeCourse.Services.Order.Application/Dtos/OrderDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FreeCourse.Services.Order.Application.Dtos
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public AddressDto Address { get; set; }
+
+        public string BuyerId { get; set; }
+
+        public List<OrderItemDto> OrderItems { get; set; }
+    }
+}
---
+++ src/OnlineCourseProjectMicroservices/Services/Order/FreeCourse.Services.Order.Infrastructure/OrderDbContext.cs
@@ -0,0 +1,29 @@
+﻿using FreeCource.Services.Order.Domain.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreeCourse.Services.Order.Infrastructure
+{
+    public class OrderDbContext : DbContext
+    {
+        public const string DEFAULT_SCHEMA = "ordering";
+
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<FreeCource.Services.Order.Domain.OrderAggregate.Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<FreeCource.Services.Order.Domain.OrderAggregate.Order>().ToTable("Orders", DEFAULT_SCHEMA);
+            modelBuilder.Entity<OrderItem>().ToTable("OrderItems", DEFAULT_SCHEMA);
+
+            modelBuilder.Entity<OrderItem>().Property(x => x.Price).HasColumnType("decimal(18,2)");
+
+            modelBuilder.Entity<FreeCource.Services.Order.Domain.OrderAggregate.Order>().OwnsOne(o => o.Address).WithOwner();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
